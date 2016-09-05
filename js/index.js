ab4f79815927866a963ed3c9b28f367dabb64e8f
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2016-09-05 07:49:16 -0500

    Inicio

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	cordova.plugins.barcodeScanner.scan(
+		function (result) {
+			alert("We got a barcode\n" +
+			"Result: " + result.text + "\n" +
+			"Format: " + result.format + "\n" +
+			"Cancelled: " + result.cancelled);
+		}, 
+		function (error) {
+			alert("Scanning failed: " + error);
+		}
+	);
+});
