ca1c2c1ad9c1a9864c48d979bc9ff57d659d7988
Hugo Luis Santiago Altamirano <hugooluisss@gmail.com>
2017-07-03 22:03:01 -0500

    Probando el background

--- js/index.js
+++ js/index.js
@@ -1,35 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+var app = {
+	// Application Constructor
+	initialize: function() {
+		this.bindEvents();
+	},
+	// Bind Event Listeners
+	//
+	// Bind any events that are required on startup. Common events are:
+	// 'load', 'deviceready', 'offline', and 'online'.
+	bindEvents: function() {
+		document.addEventListener('deviceready', this.onDeviceReady, false);
+	},
+	// deviceready Event Handler
+	//
+	// The scope of 'this' is the event. In order to call the 'receivedEvent'
+	// function, we must explicitly call 'app.receivedEvent(...);'
+	onDeviceReady: function(){
+		console.log('deviceready. resumeType=' + cordova.backgroundapp.resumeType);
+		window.plugin.backgroundMode.enable();
+		getGPS();
+		
+		function getGPS(){
+			navigator.geolocation.getCurrentPosition(function(position){
+				$.get("http://10.0.0.5/info.php", {
+					"mensaje": cordova.backgroundapp.resumeType,
+					"latitude": position.coords.latitude,
+					"longitude": position.coords.longitude,
+					"altitude": position.coords.altitude
+				}, function(){
+			
+				});
+			}, function(){
+				console.log("Error");
+			}
+		});
+	}
+};
+
 $(document).ready(function(){
-	alert("Hola Mundo");
-	
-	cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, "http://www.nytimes.com", function(success) {
-            alert("encode success: " + success);
-          }, function(fail) {
-            alert("encoding failed: " + fail);
-          }
-        );
-	
-	
-	cordova.plugins.barcodeScanner.scan(
-		function (result) {
-			alert("We got a barcode\n" +
-			"Result: " + result.text + "\n" +
-			"Format: " + result.format + "\n" +
-			"Cancelled: " + result.cancelled);
-		}, 
-		function (error) {
-			alert("Scanning failed: " + error);
-		}
-	);
-	
-	
-	my_stream = new Stream("http://209.208.97.249/mobile/live/index.m3u8", onSuccess, onError);
-	// Play audio
-	my_media.play();
-	function onSuccess() {
-    		console.log("playAudio():Audio Success");
-	}
-	function onError(error) {
-    		alert('code: '    + error.code    + '\n' +
-    			'message: ' + error.message + '\n');
-	}
+	app.initialize();
 });
